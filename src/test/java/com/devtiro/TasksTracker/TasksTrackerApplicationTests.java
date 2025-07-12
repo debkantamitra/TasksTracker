b44a585c1e6d7069e03d09f5434f3334d9f0789b
Debkanta Mitra <debkantamitra52@gmail.com>
2025-07-13 02:05:11 +0530

    init

---
+++ src/test/java/com/devtiro/TasksTracker/TasksTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiro.TasksTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
